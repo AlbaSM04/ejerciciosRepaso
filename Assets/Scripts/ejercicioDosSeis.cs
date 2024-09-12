2a9b6fcbb066d1815b400def039ef49b5b6731b4
AlbaSM04 <alba.simon.mendez@gmail.com>
2024-09-13 01:02:54 +0200

    Primer cambio (13/04/2024)

--- Assets/Scripts/ejercicioDosSeis.cs
+++ Assets/Scripts/ejercicioDosSeis.cs
@@ -2,15 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ejercicioDosCuatro : MonoBehaviour
+public class ejercicioDosSeis : MonoBehaviour
 {
-    int vidas = 0;
-
+    // Start is called before the first frame update
     void Start()
     {
-        vidas += 77;
-        vidas += 3;
-        vidas = vidas * 4;
+        
     }
 
     // Update is called once per frame
