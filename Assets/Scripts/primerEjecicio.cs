6251c125bde1af0d6dd042f266620eb89052a299
AlbaSM04 <alba.simon.mendez@gmail.com>
2024-09-12 17:01:19 +0200

    Initial commit

---
+++ Assets/Scripts/primerEjecicio.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class primerEjecicio : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("¡Hola mundo!");
+        Debug.Log("Este es el primer videojuego de Alba");
+        Debug.Log("Estoy aprendiendo C#");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Ha pasado un frame");
+        /* El motivo por el cual este mensaje se repite todo el rato
+         * es debido a que esta situadi en el Void Update y al estar 
+         * alli se repite cada frame*/
+    }
+}
