a1fe695b12abee8c06c8eaf9d54dc703839ec2aa
AlbaSM04 <alba.simon.mendez@gmail.com>
2024-09-16 17:24:48 +0200

    16/09/2024

--- Assets/Scripts/ejercicioCuatroTres.cs
+++ Assets/Scripts/ejercicioCuatroTres.cs
@@ -4,16 +4,39 @@ using UnityEngine;
 
 public class ejercicioCuatroTres : MonoBehaviour
 {
-    float cantidad;
-    string moneda;
+    [SerializeField] string moneda;
 
     void Start()
-    {
-        
+    {       
+        if (moneda == "EUROS")
+        {
+            float resultado = Convertir(5);
+            Debug.Log("Al convertir de euros a dolares tienes " + resultado + " dolares");
+        }
+
+        if (moneda == "DOLARES")
+        {
+            float resultado = Convertir(5);
+            Debug.Log("Al convertir de dolares a euros tienes " + resultado + " euros");
+        }
     }
 
-    void Convertir ()
+    float Convertir (float cantidad)
     {
+        float resultado = 0;
+
+        if (moneda == "EUROS")
+        {
+            Debug.Log("Tienes " + cantidad + " euros");
+            resultado = cantidad * 1.11f;
+        }
+
+        if (moneda == "DOLARES")
+        {
+            Debug.Log("Tienes " + cantidad + " dolares");
+            resultado = cantidad / 1.11f;
+        }
+        return resultado;
 
     }
 
