c0af08d3686a5f220a69ead81a96ba8127758466
AlbaSM04 <alba.simon.mendez@gmail.com>
2024-09-22 18:20:11 +0200

    22/09/2024

--- Assets/Scripts/EjercicioSieteDos.cs
+++ Assets/Scripts/EjercicioSieteDos.cs
@@ -2,16 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EjercicioSieteUno : MonoBehaviour
+public class EjercicioSieteDos : MonoBehaviour
 {
-    int numero = 1;
+    // Start is called before the first frame update
     void Start()
     {
-        while (numero <= 100)
-        {
-            Debug.Log ("El numero es " + numero);
-            numero++;
-        }
+        
     }
 
     // Update is called once per frame
